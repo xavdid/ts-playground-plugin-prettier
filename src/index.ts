af9d4ba7bf07ba262706ccbe9acedca54607f6e1
David Brownman <beamneocube@gmail.com>
2020-10-09 13:24:31 -0700

    add prettier

--- src/index.ts
+++ src/index.ts
@@ -1,45 +1,55 @@
-import type { PlaygroundPlugin, PluginUtils } from "./vendor/playground"
+import type { PlaygroundPlugin, PluginUtils } from "./vendor/playground";
+
+import prettier from "prettier/standalone";
+import tsPlugin from "prettier/parser-typescript";
+
+const plugins = [tsPlugin];
+
+const ERR_ID = "prettier-error";
 
 const makePlugin = (utils: PluginUtils) => {
   const customPlugin: PlaygroundPlugin = {
-    id: "example",
-    displayName: "Dev Example",
+    id: "prettier",
+    displayName: "Prettier",
     didMount: (sandbox, container) => {
-      console.log("Showing new plugin")
+      console.log("Loading prettier");
 
       // Create a design system object to handle
       // making DOM elements which fit the playground (and handle mobile/light/dark etc)
-      const ds = utils.createDesignSystem(container)
+      const ds = utils.createDesignSystem(container);
 
-      ds.title("Example Plugin")
-      ds.p("This plugin has a button which changes the text in the editor, click below to test it")
+      ds.p(
+        "Press the button to run Prettier. At this time, there's no UNDO, so be careful."
+      );
 
-      const startButton = document.createElement("input")
-      startButton.type = "button"
-      startButton.value = "Change the code in the editor"
-      container.appendChild(startButton)
+      const startButton = document.createElement("input");
+      startButton.type = "button";
+      startButton.value = "Make Pretty!";
+      container.appendChild(startButton);
 
       startButton.onclick = () => {
-        sandbox.setText("// You clicked the button!")
-      }
+        const maybeErrEl = document.getElementById(ERR_ID);
+        if (maybeErrEl) {
+          maybeErrEl.parentElement.removeChild(maybeErrEl);
+        }
+        try {
+          const result = prettier.format(sandbox.getText(), {
+            parser: "typescript",
+            plugins,
+          });
+          sandbox.setText(result);
+        } catch (e) {
+          const el = ds.p("Err!" + e.message.split("\n").join("<br/>"));
+          el.setAttribute("id", ERR_ID);
+          utils.flashHTMLElement(el);
+          // probably a syntax error
+          console.log("err", e.message);
+        }
+      };
     },
+  };
 
-    // This is called occasionally as text changes in monaco,
-    // it does not directly map 1 keyup to once run of the function
-    // because it is intentionally called at most once every 0.3 seconds
-    // and then will always run at the end.
-    modelChangedDebounce: async (_sandbox, _model) => {
-      // Do some work with the new text
-    },
+  return customPlugin;
+};
 
-    // Gives you a chance to remove anything set up,
-    // the container itself if wiped of children after this.
-    didUnmount: () => {
-      console.log("De-focusing plugin")
-    },
-  }
-
-  return customPlugin
-}
-
-export default makePlugin
+export default makePlugin;
